3e7497062c57a5e0d36132031ef4dadc0ac57cf4
Stanislav <stankhil@gmail.com>
2025-04-05 13:35:16 +0300

    Добавьте файлы проекта.

---
+++ 01.04/Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._04.Data.Entities
+{
+    public class User
+    {
+        public Guid     Id            { get; set; }
+        public string    Name         { get; set; } = null!;
+        public string    Email        { get; set; } = null!;
+        public DateTime? BirthDate    { get; set; }
+        public DateTime? RegisteredAt { get; set; }
+        public DateTime? DeletedAt    { get; set; }
+
+    }
+}
