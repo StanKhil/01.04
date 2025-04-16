b959756521119349b07e107e08114e3491abd7bb
Stanislav <stankhil@gmail.com>
2025-04-16 16:19:39 +0300

    16.04

--- 01.04/MainWindow.xaml.cs
+++ 01.04/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using _01._04.Data;
 using _01._04.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +25,7 @@ namespace _01._04
         {
             _context = new DataContext();
             InitializeComponent();
+            
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
@@ -125,6 +127,80 @@ namespace _01._04
                 .Select(ur => String.Join("\n", ur.UserAccesses
                     .Select(ua => ua.User.Name)))
                 );
+        }
+
+        private void SignUp(object sender, RoutedEventArgs e)
+        {
+            String name = nameTextBox.Text;
+            String email = emailTextBox.Text;
+            String login = loginTextBox.Text;
+            String pass = passTextBox.Password;
+
+            Guid userId = Guid.NewGuid();
+
+            User user = new()
+            {
+                Id = userId,
+                Name = name,
+                Email = email,
+                RegisteredAt = DateTime.Now,
+            };
+
+            String salt = Random.Shared.Next().ToString();
+            UserAccess userAccess = new()
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId,
+                Login = login,
+                Salt = salt,
+                Dk = kdf(salt, pass),
+                RoleId = "SelfRegistered"
+            };
+
+            _context.Users.Add(user);
+            _context.UserAccesses.Add(userAccess);
+
+            _context.SaveChanges();
+
+            MessageBox.Show("Registered");
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            String login = loginTextBoxSignIn.Text;
+            String pass = passTextBoxSignIn.Password;
+            var userAccess = _context.UserAccesses
+                .Include(ua => ua.User)
+                .FirstOrDefault(ua => ua.Login == login);
+            if (userAccess == null)
+            {
+                MessageBox.Show("User not found");
+                return;
+            }
+            String dk = kdf(userAccess.Salt, pass);
+            if (dk!=userAccess.Dk)
+            {
+                MessageBox.Show("Wrong password");
+                return;
+            }
+            MessageBox.Show($"Welcome {userAccess.User.Name}");
+        }
+
+        String kdf(String password, String salt)
+        {
+            int c = 3;
+            int dkLength = 20;
+            String t = password + salt;
+            for(int i = 0; i < c; i++)
+            {
+                t = hash(t);
+            }
+            return t[0..dkLength];
+        }
+
+        String hash(String input)
+        {
+            return Convert.ToHexString(System.Security.Cryptography.SHA1.HashData(Encoding.UTF8.GetBytes(input)));
         }
     }
 }
